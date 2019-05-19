77860ee03142f45fa5f11f1cadd23f2bddebb90b
Mirc <denacem@gmail.com>
2019-05-19 13:47:08 +0200

    Added GUI

--- Projekt/src/gui/Gui.java
+++ Projekt/src/gui/Gui.java
@@ -1,4 +1,30 @@
 package gui;
 
-public class Gui {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+public class Gui extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Button button = new Button("upload image");
+
+        Pane menu = new Pane();
+        menu.getChildren().add(button);
+
+        Scene scene = new Scene(menu, 300, 600);
+
+        stage.setScene(scene);
+        stage.setTitle("Projektli");
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
