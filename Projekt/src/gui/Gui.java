9fa5184774151fc1897f813d61d36f353923e95d
Mirc <denacem@gmail.com>
2019-05-27 16:10:54 +0200

    Text wrapping

--- Projekt/src/gui/Gui.java
+++ Projekt/src/gui/Gui.java
@@ -59,6 +59,7 @@ public class Gui extends Application {
 
         /* Textfeld zur Darstellung der Bildinformationen */
         Text infos = new Text(10,10, "something");
+        infos.setWrappingWidth(200);
 
         /* vbox for buttons*/
         VBox menuButtons = new VBox();
